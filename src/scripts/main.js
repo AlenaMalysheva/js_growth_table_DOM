3c6ec2bc3a0a1067ffd9c233be21692b6255abfb
malys@LAPTOP-DSQPC4U1 <alenamalyseva422@gmail.com>
2024-10-15 17:39:39 +0300

    solution updated

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,9 @@
 'use strict';
 
-const appendRow = document.querySelector('.append-row');
-const appendColumn = document.querySelector('.append-column');
-const deleteRow = document.querySelector('.remove-row ');
-const deleteColumn = document.querySelector('.remove-column');
+const addRowBtn = document.querySelector('.append-row');
+const addColumnBtn = document.querySelector('.append-column');
+const deleteRowBtn = document.querySelector('.remove-row ');
+const deleteColumnBtn = document.querySelector('.remove-column');
 
 const table = document.querySelector('.field');
 
@@ -13,58 +13,50 @@ let rowCount = table.rows.length;
 let columnCount = table.rows[0].cells.length;
 
 function updateBtnsStates() {
-  appendRow.disabled = rowCount >= maxCount;
-  deleteRow.disabled = rowCount <= minCount;
-  appendColumn.disabled = columnCount >= maxCount;
-  deleteColumn.disabled = columnCount <= minCount;
+  addRowBtn.disabled = rowCount >= maxCount;
+  deleteRowBtn.disabled = rowCount <= minCount;
+  addColumnBtn.disabled = columnCount >= maxCount;
+  deleteColumnBtn.disabled = columnCount <= minCount;
 }
 
-appendRow.addEventListener('click', () => {
-  if (rowCount < maxCount) {
-    const newRow = table.insertRow();
+addRowBtn.addEventListener('click', () => {
+  const newRow = table.insertRow();
 
-    for (let i = 0; i < columnCount; i++) {
-      const newCell = newRow.insertCell();
+  for (let i = 0; i < columnCount; i++) {
+    const newCell = newRow.insertCell();
 
-      newCell.textContent = '';
-    }
-
-    rowCount++;
-    updateBtnsStates();
+    newCell.textContent = '';
   }
+
+  rowCount++;
+  updateBtnsStates();
 });
 
-deleteRow.addEventListener('click', () => {
-  if (rowCount > minCount) {
-    table.deleteRow(-1);
+deleteRowBtn.addEventListener('click', () => {
+  table.deleteRow(-1);
 
-    rowCount--;
-    updateBtnsStates();
-  }
+  rowCount--;
+  updateBtnsStates();
 });
 
-appendColumn.addEventListener('click', () => {
-  if (columnCount < maxCount) {
-    for (let i = 0; i < rowCount; i++) {
-      const cell = table.rows[i].insertCell();
+addColumnBtn.addEventListener('click', () => {
+  for (let i = 0; i < rowCount; i++) {
+    const cell = table.rows[i].insertCell();
 
-      cell.textContent = '';
-    }
-
-    columnCount++;
-    updateBtnsStates();
+    cell.textContent = '';
   }
+
+  columnCount++;
+  updateBtnsStates();
 });
 
-deleteColumn.addEventListener('click', () => {
-  if (columnCount > minCount) {
-    for (let i = 0; i < rowCount; i++) {
-      table.rows[i].deleteCell(-1);
-    }
-
-    columnCount--;
-    updateBtnsStates();
+deleteColumnBtn.addEventListener('click', () => {
+  for (let i = 0; i < rowCount; i++) {
+    table.rows[i].deleteCell(-1);
   }
+
+  columnCount--;
+  updateBtnsStates();
 });
 
 updateBtnsStates();
